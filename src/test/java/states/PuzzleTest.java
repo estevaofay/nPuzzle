51ec82197cc8778805e24b1f0a928dd816d67a40
Estêvão Francisco Fáy <eff.fay@gmail.com>
2021-05-30 10:44:28 -0300

    Add Unit Tests

--- src/test/java/states/PuzzleTest.java
+++ src/test/java/states/PuzzleTest.java
@@ -40,4 +40,47 @@ class PuzzleTest {
         final Puzzle puzzle = new Puzzle(3);
         assertFalse(puzzle.move(Puzzle.Direction.UP).isGoal());
     }
+
+    @Test
+    void moveUp() {
+        int[][] expectedMatrix = new int[3][3];
+        expectedMatrix[0][0] = 1;
+        expectedMatrix[0][1] = 2;
+        expectedMatrix[0][2] = 3;
+        expectedMatrix[1][0] = 4;
+        expectedMatrix[1][1] = 5;
+        expectedMatrix[1][2] = 9;
+        expectedMatrix[2][0] = 7;
+        expectedMatrix[2][1] = 8;
+        expectedMatrix[2][2] = 6;
+
+        final Puzzle puzzle = new Puzzle(3);
+        assertMatricesAreEqual(expectedMatrix, puzzle.move(Puzzle.Direction.UP).getMatrix());
+
+    }
+
+    @Test
+    void moveDownFromDefaultState() {
+        final Puzzle puzzle = new Puzzle(3);
+        assertNull(puzzle.move(Puzzle.Direction.DOWN));
+    }
+
+    @Test
+    void moveDown() {
+        int[][] expectedMatrix = new int[3][3];
+        expectedMatrix[0][0] = 1;
+        expectedMatrix[0][1] = 2;
+        expectedMatrix[0][2] = 3;
+        expectedMatrix[1][0] = 4;
+        expectedMatrix[1][1] = 5;
+        expectedMatrix[1][2] = 9;
+        expectedMatrix[2][0] = 7;
+        expectedMatrix[2][1] = 8;
+        expectedMatrix[2][2] = 6;
+
+        final Puzzle puzzle = new Puzzle(3);
+        final int[][] actualMatrix = puzzle.move(Puzzle.Direction.UP).move(Puzzle.Direction.UP).move(Puzzle.Direction.DOWN).getMatrix();
+
+        assertMatricesAreEqual(expectedMatrix, actualMatrix);
+    }
 }
