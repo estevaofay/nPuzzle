d5bf338f458cc989844d120185ed242f4db8e612
Estêvão Francisco Fáy <eff.fay@gmail.com>
2021-05-29 23:28:28 -0300

    Refactor and add test class

--- src/main/java/Search.java
+++ src/main/java/Search.java
@@ -7,8 +7,8 @@ public class Search {
     public static void main(String[] args) {
 
         Puzzle startPuzzle = new Puzzle(3);
-        AStarAlgorithm s = new AStarAlgorithm(startPuzzle.getNode());
-        Node solved = s.search();
+        AStarAlgorithm aStarAlgorithm = new AStarAlgorithm(startPuzzle.getNode());
+        Node solved = aStarAlgorithm.search();
 
     }
 }
